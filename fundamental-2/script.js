4eea7abc6f02de2e5d0f7863bf424843971815ea
linganand-04 <linganandb2004@gmail.com>
2025-01-19 20:34:50 +0530

    array finished..

--- fundamental-2/script.js
+++ fundamental-2/script.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /* // 003 FUNCTIONS // 
 
@@ -146,6 +146,7 @@ HINT: To check if number A is at least double number B, check for A >= 2 * B. Ap
 // const DolphinAvg = (44+23+71)/3;
 // const KoalasAvg = (65+54+49)/3
 
+/*
 const calcAverage = (a,b,c) => (a+b+c)/3
 console.log(calcAverage(10,10,10));
 
@@ -179,20 +180,146 @@ console.log(DolphinsScore,KoalasScore);
 
 checkWinner(DolphinsScore,KoalasScore)
 
+*/
 
+// 008 INTRODUCTION TO ARRAYS
+/*
+const friend1 = "Linganand";
+const friend2 = "Sanjay";
+const friend3 = "Arun";
+const friend4 = "Balaji";
 
+const friend = ["Linganand", "Sanjay", "Arun", "Balaji"];
 
+console.log(friend);
 
+console.log(friend[0]);
+console.log(friend[3]);
+console.log(friend[1]);
 
+const years = new Array(2004, 2004, 2004, 2005);
+console.log(years);
 
+console.log(friend.length);
 
+console.log(friend[friend.length - 1]);
 
+friend[3] = "Mathan Raj";
+console.log(friend);
 
+// If you try to change the value of array..First check the variables..
+// If const variables do not change the value
 
+// friend = ["paiya", "dei"]
+// console.log(friend);
 
+const firstname = "Linga";
+const ling = [firstname, "Lian", 2025 - 2004, friend];
+console.log(ling);
 
+console.log(ling.length);
 
+// Exercise
 
+const calcage = function (birthyear) {
+  return 2025 - birthyear;
+};
 
+const y = [2024, 2020, 2016, 2005, 2004];
 
+console.log(y);
+
+const age1 = calcage(y[0]);
+const age2 = calcage(y[1]);
+const age3 = calcage(y.length - 1);
+
+const age = [calcage(y[0]), calcage(y[2]), calcage(y[3]), y[3]];
+*/
+
+// 011 - BASIC ARRAY OPERATIONS (METHODS)
+/*
+const friend = ["Linganand", "Sanjay", "Arun", "Balaji"];
+console.log(friend);
+
+// Add elements
+friend.push("Mathan"); // Add - Mathan at end of array
+console.log(friend);
+
+friend.unshift("Gokul"); // Add - Gokul at start of the array
+console.log(friend);
+
+// Remove elements
+
+friend.pop();
+console.log(friend); // Remove - last array elements from the friend
+
+friend.shift();
+console.log(friend); 
+
+const popped = friend.shift();
+console.log(popped); 
+
+console.log(friend);
+
+// Remember that indexOf is case sensistive 
+
+console.log(friend.indexOf("Sanjay")); // Check the "Sanjay" in a array friends = ans. 0
+
+// if doesn't exist = ans. -1
+
+// Remember that includes is case sensistive 
+
+console.log(friend.includes("Arun")); // true
+console.log(friend.includes("arun")); // false
+
+if (friend.includes("balaji")) {
+  console.log("Balaji is your friend list");
+}
+else{
+  console.log(`Balaji is Stranger..`);
+}
+*/
+
+// 011 - CHALLENGE #2
+
+/*
+
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of 
+the bill if the bill value is between 50 and 300, and if the value is different, the tip is 
+20%.
+
+Your tasks:
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
+
+2. And now let's use arrays! So create an array 'bills' containing the test data below
+
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before
+
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip 
+
+Test data: 125, 555 and 44
+
+Hint: Remember that an array needs a value in each position and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array)
+
+*/
+
+// const calcTip = function (bill) {
+//   return bill => 50 && bill <= 300 ? bill * 0.15 : bill * 0.2 ;
+// }
+
+const calcTip = bill => 50 && bill <= 300 ? bill*0.15 : bill*0.2 
+
+const bills = [125,555,44]
+
+const tips = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])]
+
+console.log(bills,tips);
+
+// Doesn't Work..
+// const total = [bills.values+tips.values]
+
+const total = [bills[0]+tips[0],bills[1]+tips[1],bills[2]+tips[2]]
+
+console.log(`Total = ${total}`);
 
