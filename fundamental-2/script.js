5b2fafa6445b8829d019fa55959e00d6a8f145b4
linganand-04 <linganandb2004@gmail.com>
2025-01-24 17:08:09 +0530

    READY to start Challenge-4..

--- fundamental-2/script.js
+++ fundamental-2/script.js
@@ -600,7 +600,7 @@ for (let i = 0; i <= lian.length; i++) {
 
 // 018 - LOOPING BACKWARDS AND LOOP IN LOOP
 
-
+/*
 // THIS IS HARD CODE.. Don't do like this..❌
 
 // for(let int=10; int>=0; int-- ){
@@ -632,5 +632,59 @@ for( let int = 1 ; int<=3; int++){
     
   }
 }
+*/
 
+// 019 - THE WHILE LOOP 
 
+/*
+// FOR LOOP
+// for(let int=1; int<=5; int++ ){
+//   console.log(`Current Number is (${int}) next number is ${int+1}`);
+// }
+
+// WHILE LOOP
+// let a = 0;
+// while(a<=10){
+//   console.log(a);
+//   a++
+// }
+
+let dice = Math.trunc(Math.random() * 6)+1;
+console.log(dice);
+
+while (dice===6) {
+  console.log(`You roll the value ${dice}`);
+  dice = Math.trunc(Math.random() * 6)+1;
+  if(dice!==6){
+    console.log(`Your roll extact value ${dice} so..END`);
+    
+  }
+}
+*/
+
+// 020 - Coding Challenge #4 
+
+/*
+Let's improve Steven's tip calculator even more, this time using loops! 
+
+Your tasks: 
+
+1. Create an array 'bills' containing all 10 test bill values 
+
+2. Create empty arrays for the tips and the totals ('tips' and 'totals') 
+
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations! Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52 
+
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the tips and totals arrays 😉 
+
+Bonus: 
+
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a difficult challenge (we haven't done this before)! Here is how to solve it: 
+
+4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together 
+
+4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements) 
+
+4.3. Call the function with the 'totals' array 
+
+*/
