2fbe1e12889d89b13ee14eed6e2709a8251de3fc
linganand-04 <linganandb2004@gmail.com>
2025-01-27 17:26:22 +0530

    Dom Process..

--- 4-DOM and Events Fundamentals/script.js
+++ 4-DOM and Events Fundamentals/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+
+console.log(
+  (document.querySelector('.message').textContent = 'Correct Answer...')
+);
+
+console.log((document.querySelector('.number').textContent = 13));
+
+console.log((document.querySelector('.score').textContent = 19));
+
+document.querySelector('.guess').value;
+console.log((document.querySelector('.guess').value = 24));
+*/
+
+const guessNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = guessNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🙄 No Number';
+  } else if (guessNumber === guess) {
+    document.querySelector('.message').textContent = '💥 Correct Number..';
+  } else if (guessNumber < guess) {
+    document.querySelector('.message').textContent = 'Too High..📈';
+  } else if (guessNumber > guess) {
+    document.querySelector('.message').textContent = 'Too Low..📉';
+  }
+});
