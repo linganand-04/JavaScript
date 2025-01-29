e9a818d592d4a07b3d6b273050d7c29a9a74739a
linganand-04 <linganandb2004@gmail.com>
2025-01-29 17:01:07 +0530

    Guess My Number Finished..

--- 4-DOM and Events Fundamentals/script.js
+++ 4-DOM and Events Fundamentals/script.js
@@ -17,41 +17,75 @@ console.log((document.querySelector('.guess').value = 24));
 
 let guessNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
+
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
 // document.querySelector('.number').textContent = guessNumber;
 
 // Reset the GAME
 document.querySelector('.again').addEventListener('click', function () {
-  score = 20
-  // fixx the bug in this game 
-  /*
-  document.querySelector('.number').value = guessNumber;
-  */
+  score = 20;
+  guessNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.score').textContent = '20';
+  document.querySelector('body').style.background = '#222';
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.message').textContent = 'Start guessing...';
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.number').textContent = '?';
-  const guess = Number(document.querySelector('.guess').value = '' );
+  const guess = Number((document.querySelector('.guess').value = ''));
 });
 // --------------
-
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('body').style.background = '#222';
 
   // if there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '🙄 No Number';
+    // document.querySelector('.message').textContent = '🙄 No Number';
+    displayMessage('🙄 No Number');
   }
 
+  if (guess <= 0) {
+    // document.querySelector('.message').textContent = 'Between 1 and 20 ⚠';
+    displayMessage('Between 1 and 20 ⚠');
+    document.querySelector(
+      'body'
+    ).style.background = `linear-gradient(rgb(143, 47, 47), black)`;
+  }
 
   // when player wins
   else if (guessNumber === guess) {
-    document.querySelector('.message').textContent = '💥 Correct Number..';
+    // document.querySelector('.message').textContent = '💥 Correct Number..';
+    displayMessage('💥 Correct Number..');
     document.querySelector('.number').textContent = guessNumber;
     document.querySelector('body').style.backgroundColor = '#163825';
+    document.querySelector('body').style.background = '#163825';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = score;
+    }
+  } else if (guessNumber !== guess) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent = guessNumber < guess ? 'Too High..📈' : 'Too Low..📉';
+      displayMessage(guessNumber < guess ? 'Too High..📈' : 'Too Low..📉');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // document.querySelector('.message').textContent = 'You lost the Game..☠';
+      displayMessage('You lost the Game..☠');
+      document.querySelector('.number').textContent = guessNumber;
+      document.querySelector('body').style.backgroundColor = '#7A2020';
+      document.querySelector('body').style.background = '#7A2020';
+    }
   }
 
+  /*
   // when guess is too high
   else if (guessNumber < guess) {
     if (score > 1) {
@@ -77,16 +111,13 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('body').style.backgroundColor = '#7A2020';
     }
   }
-
-  // fixx the bug in this game 
-
-  /*
-  else if (guess<20){
-    document.querySelector('.message').textContent = 'Between 1 and 20 ⚠';
-  }
-
-  else if (guess>0){
-    document.querySelector('.message').textContent = 'Between 1 and 20 ⚠';
-  }
   */
+  if (guess > 20) {
+    console.log(guess);
+    // document.querySelector('.message').textContent = 'Between 1 and 20 ⚠';
+    displayMessage('Between 1 and 20 ⚠');
+    document.querySelector(
+      'body'
+    ).style.background = `linear-gradient(rgb(143, 47, 47), black)`;
+  }
 });
