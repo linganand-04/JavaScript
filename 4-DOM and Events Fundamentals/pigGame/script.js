d1d59616b252013eca1a2a285cdaf75cbb52417b
linganand-04 <linganandb2004@gmail.com>
2025-01-30 17:29:15 +0530

    Dice

--- 4-DOM and Events Fundamentals/pigGame/script.js
+++ 4-DOM and Events Fundamentals/pigGame/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const roll = document.querySelector('.btn--roll');
+const diceImg = document.querySelector('.dice');
+
+const dummy = function () {
+  var img = Math.floor(Math.random() * 6) + 1;
+  console.log(img);
+
+  diceImg.src = `img/dice-${img}.png`;
+};
+
+roll.addEventListener('click', dummy);
