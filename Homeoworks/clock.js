c95b70644887406756b77b518d3832e087a5e4e0
linganand-04 <linganandb2004@gmail.com>
2025-02-21 16:57:30 +0530

    Ready to start For of video

--- Homeoworks/clock.js
+++ Homeoworks/clock.js
@@ -1,17 +1,22 @@
-'use strict'
+"use strict";
 
-const hours = document.querySelector('.hours')
-const minutes = document.querySelector('.minutes')
-const seconds = document.querySelector('.seconds')
+const hours = document.querySelector(".hours");
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+const noon = document.querySelector(".noon");
 
 function clock() {
-  const hrs = new Date().getHours()
-  const min = new Date().getMinutes()
-  const sec= new Date().getSeconds()
+  const date = new Date();
+  const hrs = date.getHours() < 12 ? `0${date.getHours()}` : date.getHours();
+  const min =
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+  const sec =
+    date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
   // console.log(`${hrs}:${min}:${sec}`);
-  hours.innerHTML = hrs
-  minutes.innerHTML = min
-  seconds.innerHTML = sec
-  }
+  hours.innerHTML = hrs;
+  minutes.innerHTML = min;
+  seconds.innerHTML = sec;
+  noon.innerHTML = date.getHours() < 12 ? "AM" : "PM";
+}
 
-  setInterval(clock,1000)
+setInterval(clock, 1000);
